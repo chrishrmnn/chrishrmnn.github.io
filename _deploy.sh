690742866fde95f401b5cfcf9a13b211442dd89f
chrishrmnn <contact@christophe-hermann.fr>
2016-02-03 22:33:20 +0100

    Cleaning ...

--- _deploy.sh
+++ _deploy.sh
@@ -8,6 +8,7 @@ if [ $response = "y" ]
 then
 	git branch -D master
 	git checkout -b master
+	echo "http://www.christophe-hermann.fr" > _site/CNAME
 	git filter-branch --subdirectory-filter _site/ -f
 	git push github master
 # If 'N', don't do the job and exit.
